33dd14df61c56699afa8b863b8737ea076f34875
ishan <ishan4220@gmail.com>
2021-06-23 18:07:15 +0530

    converted_to_typesciprt

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,17 @@
-import {Router} from 'express';
-import puppeteer from 'puppeteer';
+import { Router } from "express";
+import puppeteer from "puppeteer";
 
 const router: Router = Router();
 
 //Server side rendering of the front page of the react app using puppeteer
-router.get('/', async (req,res)=>{
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-    await page.waitForSelector('#task-list');
-    let data = await page.content();
-    await browser.close();
-    return res.send(data);
-})
+router.get("/", async (req, res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+  await page.waitForSelector("#task-list");
+  let data = await page.content();
+  await browser.close();
+  return res.send(data);
+});
 
-export default router
+export default router;
